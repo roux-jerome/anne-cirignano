065389ab6d763ddf990bb6c5989004191763337a
Jérôme Roux <jerome.roux@pole-emploi.fr>
2023-03-26 17:42:23 +0200

    build(ressource): limites le nombre de ressources

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack');
+
+const devMode = process.env.NODE_ENV !== "production";
+
+module.exports = {
+    entry: {
+        main: './src/main.js',
+        style: './src/style.css',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        }),
+        new MiniCssExtractPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },{
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
